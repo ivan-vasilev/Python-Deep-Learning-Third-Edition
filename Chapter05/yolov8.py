dc5fd3bd45ed8c6e94d548fec2416a32e9216532
ivan-vasilev <ivanvasilev@gmail.com>
2023-09-14 01:39:26 +0300

    yolov8 fix + jupyter notebook

--- Chapter05/yolov8.py
+++ Chapter05/yolov8.py
@@ -4,9 +4,9 @@ from ultralytics import YOLO
 model = YOLO("yolov8n.pt")
 
 # Detect objects on a Wikipedia image
-results = model.predict('https://github.com/ivan-vasilev/Python-Deep-Learning-3rd-Edition/blob/main/Chapter05/wikipedia-2011_FIA_GT1_Silverstone_2.jpg?raw=true')
+results = model.predict('https://raw.githubusercontent.com/ivan-vasilev/Python-Deep-Learning-3rd-Edition/main/Chapter05/wikipedia-2011_FIA_GT1_Silverstone_2.jpg')
 
 # convert results->numpy_array->Image and display it
 from PIL import Image
 
-im = Image.fromarray(results[0].plot()).show()
+Image.fromarray(results[0].plot()).show()
